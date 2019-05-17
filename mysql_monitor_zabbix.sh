c77c7ddfd5aeb98db084d6e3adca55edfb82abf2
dragutin-mcloud <45963405+dragutin-mcloud@users.noreply.github.com>
2019-05-17 11:59:23 +0200

    Update mysql_monitor_zabbix.sh

--- mysql_monitor_zabbix.sh
+++ mysql_monitor_zabbix.sh
@@ -2,5 +2,6 @@
 
 sudo apt install -y jq mysql-client-core-5.7
 echo "UserParameter=Mysql.Server-Status, mysql --defaults-file=/etc/zabbix/.my.cnf --defaults-group-suffix=_monitor" > /etc/zabbix/zabbix_agentd.d/userparameter_mysql_template.conf
-echo "" > /etc/zabbix/.my.cnf
+#echo "" > /etc/zabbix/.my.cnf
+cp /root/.my.cnf /etc/zabbix/.my.cnf
 service zabbix-agent restart
